124e1c36f6ad9574a4e3b619ea12fd08c0f3ba61
unknown <tyknightstudio@gmail.com>
2020-10-19 20:37:54 +0700

    up project

---
+++ DataBase/db_java.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th7 14, 2020 lúc 06:24 AM
+-- Phiên bản máy phục vụ: 10.4.11-MariaDB
+-- Phiên bản PHP: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `db_java`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nv`
+--
+
+CREATE TABLE `nv` (
+  `manv` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+  `tennv` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `ngaysinh` date NOT NULL,
+  `ten_chucvu` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `hesoluong` float NOT NULL,
+  `hesophucap` float NOT NULL,
+  `tong_luong` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `nv`
+--
+ALTER TABLE `nv`
+  ADD PRIMARY KEY (`manv`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
